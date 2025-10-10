72b7afae9a9f0ad8dc0c45ded9b7688836fcb66d
JM Repos <jawdatjm@gmail.com>
2025-10-10 16:13:43 +1100

    replace jest_config.mjs with jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+export default {
+ testEnvironment: 'node',
+ transform: {}, // required for ESM support
+}
+ /*
+ or do in in the config file :
+
+ reporters: ["default", "github-actions"],
+
+ */
