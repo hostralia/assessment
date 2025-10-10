39523e1ab6841804417606bfc4370831bb9ae16c
JM Repos <jawdatjm@gmail.com>
2025-10-10 14:49:54 +1100

    variuos updates

--- practical/docs.js
+++
@@ -1,9 +0,0 @@
-// One-line combined command
-
-// git add . && git commit -m "Your commit message" && git push
-
-
-// or:
-// git config --global alias.acp '!git add . && git commit -m "$1" && git push' 
-// then:
-// git acp "<<your message" 
